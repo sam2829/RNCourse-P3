ad4aecf646b35dccc6816a3a5a9c28bd8c3b8d5c
sam2829 <samscarisbrick@outlook.com>
2025-04-18 12:30:13 +0100

    Add navigation between two page. pass and filter data in meal overview page.

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,34 @@
+import { FlatList, View, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedmeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedmeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
